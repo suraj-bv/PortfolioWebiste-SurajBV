f35b439a27c088fecb030924ce3ebd591070b36f
suraj-bv <surajbv5566@gmail.com>
2024-11-27 19:12:53 +0530

    Js Practice

--- scripts.js
+++ scripts.js
@@ -1,19 +1,3 @@
-let n = prompt('Enter the number: ');
-let arr = []
-for(let i = 1; i <= n; i++ ){
-    arr[i-1] = i
-}
+name = prompt('Enter your name:');
 
-console.log(arr)
-
-let sum = arr.reduce((res, cur)=>{
-    return res + cur
-});
-
-console.log(sum)
-
-let mul = arr.reduce((res, cur)=>{
-    return res * cur
-});
-
-console.log(mul)
+console.log(name)
